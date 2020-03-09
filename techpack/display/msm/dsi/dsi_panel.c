9d1f376234bf862f0cb831dcb3d48dc22580ce19
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    techpack: display: msm: Adapt exposure adjustment methods for sm8250
    - This commit was originally made for davinci but it helps for us too as it retains backlight level prior to enabling DC Dim.
    
    [muralivijay: Adapt to spes]
    [fadlyas07: Remove config guards.]
    [johnmart19: Adapt for sm8250]
    
    Co-authored-by: muralivijay <muralidharan9845@gmail.com>
    Co-authored-by: fadlyas07 <mhmmdfdlyas@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -932,7 +932,6 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	if (panel->host_config.ext_bridge_mode)
 		return 0;
-
         if (bl_lvl > 0)
                 bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
 
