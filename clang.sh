60569b2a5c2e2559e4a8d63fd1f7d7654230a325
olzhas0986 <138654744+olzhas0986dev@users.noreply.github.com>
2025-06-15 07:37:37 +0000

    Add AOSP clang installer script

---
+++ clang.sh
@@ -0,0 +1,11 @@
+# AOSP Clang & gcc linaro installer script
+mkdir clang
+cd clang
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz
+tar -xvf *.gz
+rm -rf *.tar.gz
+
+cd ..
+wget https://snapshots.linaro.org/gnu-toolchain/13.0-2022.10-1/aarch64-linux-gnu/gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
+tar -xf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
+rm -rf gcc-linaro-13.0.0-2022.10-x86_64_aarch64-linux-gnu.tar.xz
